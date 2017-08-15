e8e708121faca29c1626d7a29519028fe287098a
tefy22 <tifiana_1991@hotmail.com>
2017-08-15 09:06:15 -0500

    Final1

--- index.js
+++ index.js
@@ -1,10 +1,101 @@
 var hoteles=[];	
 var latG, lngG;
+var marcador;	
+var contadorId=1;
 
-$(document).ready(function () {
+//---------------CAMBIAR DE PAGINA--------------------
+function cambiarPagina(page){
+	$.mobile.changePage("#"+page,{
+		transition:'flow'
+	});
+}
+//-----------------LIMPIAR CAMPOS---------------------
+function limpiarCampos(){
+	$("#nombre").val("");
+	$("#ciudad").val("");
+	$("#telefono").val("");
+	var selectEstrellas= $("#estrellas");
+	selectEstrellas[0].selectedIndex=0;
+	selectEstrellas.selectmenu('refresh');	
+}
+//------------------MOSTRAR DATOS AL SELECCIONAR UN DATO--------------------
+function mostrarDatos(id){
+	cambiarPagina('pagDetalle');
+	var div= $("#datos");
 
-	var marcador;	
-	var contadorId=1;
+	for (var i=0; i<hoteles.length; i++) {
+		if (id==hoteles[i].id){
+			var datos="";
+			datos+="<b>Nombre: </b>"+hoteles[i].nombre+"<br>";
+			datos+="<b>Ciudad: </b>"+hoteles[i].ciudad+"<br>";
+			datos+="<b>Telefono: </b>"+hoteles[i].telefono+"<br>";
+			datos+="<b>Estrellas: </b>"+hoteles[i].estrellas+"<br>";
+			div.html(datos);
+
+			var latlngNuevo= new google.maps.LatLng(hoteles[i].latitud, hoteles[i].longitud);
+			var opciones={
+				zoom:5,
+				center: latlngNuevo,
+				mapTypeId: google.maps.MapTypeId.ROADMAP
+			}
+			var mapa= new google.maps.Map(document.getElementById('divMapa2'),opciones);
+			var marcadorNuevo= new google.maps.Marker({
+				position: latlngNuevo,
+				map: mapa,
+				title:"Mi nueva ubicacion"
+			});
+			
+		}else{
+			console.log("No encontrado")
+		}
+	}	
+}
+//--------------MAPA---------------------------------
+function mostrarMapa(posicion){
+	var opciones={
+		zoom:5,
+		center:posicion,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	}
+	var mapa= new google.maps.Map(document.getElementById('divMapa'),opciones);
+	marcador= new google.maps.Marker({
+		position: posicion,
+		map: mapa,
+		draggable: true,
+		title:"Mi Ubicacion"
+	});
+	google.maps.event.addListener(marcador,'dragend',function(event){
+		latG= event.latLng.lat();//se guarda en las respectivas variables la nueva posicion del marcador
+		lngG= event.latLng.lng();//se guarda en las respectivas variables la nueva posicion del marcador
+		console.log(latG);
+	})
+}
+function exito(pos){
+	var latlng= new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
+	mostrarMapa(latlng);
+}
+function fallida(error){
+	if (error.code==0){
+		alert("No se puede obtener la posicion actual");
+	}else if (error.code==1){
+		alert("Algo ha salido mal!");
+	}else if (error.code==2){
+		alert("No has aceptado compartir tu ubicacion");
+	}else if (error.code==3){
+		alert("Hemos superado el tiempo de espera");
+	}
+}
+function obtenerDirActual(){
+	if (navigator.geolocation){
+		navigator.geolocation.getCurrentPosition(exito, fallida,{
+			maximumAge: 500000,
+			enableHighAccuracy: true,//que tenga alta precision
+            timeout: 6000
+		});
+	}
+}
+//---------------------------------------------------------------------------
+$(document).ready(function () {	
 	
 	$("#btnRegistrarHotel").click(function(){
 		obtenerDirActual();
@@ -39,116 +130,33 @@ $(document).ready(function () {
 		var telefono= $("#telefono").val();
 		var estrellas= $("#estrellas").val();
 
-		var hotel={
-			id: id,
-			nombre: nombre,
-			ciudad: ciudad,
-			telefono: telefono,
-			estrellas: estrellas,
-			latitud: latG,
-			longitud: lngG
-		};
-		hoteles.push(hotel);
-		alert("Datos Guardados!");
-		limpiarCampos();
-		contadorId++;
-		obtenerDirActual();		
-	});
-
-
-});
-//---------------CAMBIAR DE PAGINA--------------------
-function cambiarPagina(page){
-	$.mobile.changePage("#"+page,{
-		transition:'flow'
-	});
-}
-//-----------------LIMPIAR CAMPOS---------------------
-function limpiarCampos(){
-	$("#nombre").val("");
-	$("#ciudad").val("");
-	$("#telefono").val("");
-	var selectEstrellas= $("#estrellas");
-	selectEstrellas[0].selectedIndex=0;
-	selectEstrellas.selectmenu('refresh');	
-}
-//------------------MOSTRAR DATOS AL SELECCIONAR UN DATO--------------------
-function mostrarDatos(id){
-	cambiarPagina('pagDetalle');
-	var div= $("#datos");
-
-	for (var i=0; i<hoteles.length; i++) {
-		if (id==hoteles[i].id){
-			var datos="";
-			datos+="<b>Nombre: </b>"+hoteles[i].nombre+"<br>";
-			datos+="<b>Ciudad: </b>"+hoteles[i].ciudad+"<br>";
-			datos+="<b>Telefono: </b>"+hoteles[i].telefono+"<br>";
-			datos+="<b>Estrellas: </b>"+hoteles[i].estrellas+"<br>";
-			div.append(datos);
-
-			var latlngNuevo= new google.maps.LatLng(hoteles[i].latitud, hoteles[i].longitud);
-			mostrarMapaNuevo(latlngNuevo);
+		if (nombre.trim().length==0 || ciudad.trim().length==0 || telefono.trim().length==0 
+			|| estrellas.trim().length==0){
+			alert("Debes todos los campos");
+			$("#nombre, #ciudad, #telefono").css({
+				"backgroundColor":"#F9DFDE",
+				"border":"1px solid red"
+			});
 		}else{
-			console.log("No encontrado")
-		}
-	}	
-}
-//--------------MAPA---------------------------------
-function mostrarMapa(posicion){
-	var opciones={
-		zoom:5,
-		center:posicion,
-		mapTypeId: google.maps.MapTypeId.ROADMAP
-	}
-	var mapa= new google.maps.Map(document.getElementById('divMapa'),opciones);
-	marcador= new google.maps.Marker({
-		position: posicion,
-		map: mapa,
-		draggable: true,
-		title:"Mi Ubicacion"
+			$("#nombre, #ciudad, #telefono").css({
+				"backgroundColor":"white",
+				"border":"none"
+			});
+			var hotel={
+				id: id,
+				nombre: nombre,
+				ciudad: ciudad,
+				telefono: telefono,
+				estrellas: estrellas,
+				latitud: latG,
+				longitud: lngG
+			};
+			hoteles.push(hotel);
+			alert("Datos Guardados!");
+			limpiarCampos();
+			contadorId++;
+		}				
 	});
-	google.maps.event.addListener(marcador,'dragend',function(event){
-		latG= event.latLng.lat();
-		lngG= event.latLng.lng();
-		console.log(latG);
-	})
-}
-function exito(pos){
-	var latlng= new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
-	mostrarMapa(latlng);
-}
-function fallida(error){
-	if (error.code==0){
-		alert("No se puede obtener la posicion actual");
-	}else if (error.code==1){
-		alert("Algo ha salido mal!");
-	}else if (error.code==2){
-		alert("No has aceptado compartir tu ubicacion");
-	}else if (error.code==3){
-		alert("Hemos superado el tiempo de espera");
-	}
-}
-function obtenerDirActual(){
-	if (navigator.geolocation){
-		navigator.geolocation.getCurrentPosition(exito, fallida,{
-			maximumAge: 500000,
-			enableHighAccuracy: true,//que tenga alta precision
-            timeout: 6000
-		});
-	}
-}
-//----------------------MOSTRAR MAPA NUEVO--------------------------
-function mostrarMapaNuevo(posNueva){
-	var opciones={
-		zoom:5,
-		center: posNueva,
-		mapTypeId: google.maps.MapTypeId.ROADMAP
-	}
-	var mapa= new google.maps.Map(document.getElementById('divMapa2'),opciones);
-	var marcadorNuevo= new google.maps.Marker({
-		position: posNueva,
-		map: mapa,
-		title:"Mi Ubicacion"
-	});
-}
+});
+
 
